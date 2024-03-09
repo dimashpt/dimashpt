6ea870c60f8a6e7803884b0fb5db4894dd3e8162
Dimas <dimashpt@gmail.com>
2024-03-09 14:15:01 +0700

    feat: add github link in homepage

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -14,9 +14,18 @@ const RootPage: React.FC<BasePageProps> = async ({
         <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold">
           {t('common:name')}
         </h1>
-        <h5 className="text-base md:text-2xl mt-2 text-muted-foreground">
+        <h5 className="text-base md:text-2xl mt-2 mb-5 text-muted-foreground">
           &gt; {t('common:role')}
         </h5>
+        <div className="space-y-1 text-sm text-muted-foreground">
+          <p>{t('code_comment')}</p>
+          <p>
+            {Array.from({ length: 3 }).map((_, i) => (
+              <span key={i}>{t(`code_github.${i}`)}</span>
+            ))}
+            <a href={t('code_github.3')}>&quot;{t('code_github.3')}&quot;</a>
+          </p>
+        </div>
       </div>
     </div>
   );
